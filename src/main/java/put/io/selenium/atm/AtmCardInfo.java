98f8c36f2c2786de5a31ee158f509b39c8641c14
Oleg Rudnev <129340789+rudofficial@users.noreply.github.com>
2023-11-03 02:10:23 +0300

    Update AtmCardInfo.java

--- src/main/java/put/io/selenium/atm/AtmCardInfo.java
+++ src/main/java/put/io/selenium/atm/AtmCardInfo.java
@@ -8,7 +8,7 @@ package put.io.selenium.atm;
  */
 public class AtmCardInfo {
 
-	final int RIGHT_PIN = 1520; //change pin
+	final int RIGHT_PIN = 1525; //change pin
 	
 	boolean isCardIn = false;
 	
